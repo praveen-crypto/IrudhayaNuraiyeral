7e2025451cb563923416693fb003a0828e8050a5
praveen-crypto <kumarspraveen57@gmail.com>
2020-07-09 12:31:51 +0530

    Removed unnecessary files

---
+++ BloodSim.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+from HomeWindow import Ui_HomeWindow
+from Mainwindow import Heart_para, Steno_para, Ui_MainWindow
+
+
+class Controller():
+
+    def Show_HomeWindow(self):
+        self.HomeWindow = QtWidgets.QMainWindow()
+        self.ui = Ui_HomeWindow(self.HomeWindow)
+        self.ui.pushButton_1.clicked.connect(self.Show_SecondWindow)
+        self.ui.pushButton_2.clicked.connect(self.manual)
+        self.ui.pushButton_3.clicked.connect(self.exit)
+        self.HomeWindow.show()
+
+    def exit(self):
+        self.HomeWindow.close()
+
+    def manual(self):
+        path = os.path.join('Manual.pdf')
+        os.startfile(path)
+
+    def Show_SecondWindow(self):
+        try:
+            self.MainWindow = QtWidgets.QMainWindow()
+            self.ui = Ui_MainWindow(self.MainWindow)
+            self.ui.actionQuit.triggered.connect(self.exit1)
+            self.ui.pushButton_HP.clicked.connect(self.heart_para)
+            self.ui.pushButton_STENO.clicked.connect(self.Steno_para)
+            self.MainWindow.show()
+            self.HomeWindow.close()
+        except Exception as e:
+            alert = QtWidgets.QMessageBox()
+            alert.setText(str(e))
+            alert.exec_()
+
+    def exit1(self):
+        self.MainWindow.close()
+
+    def heart_para(self):
+        self.widget_1 = QtWidgets.QWidget()
+        self.wui = Heart_para(self.widget_1)
+        self.widget_1.show()
+
+    def Steno_para(self):
+        self.Form = QtWidgets.QWidget()
+        self.sui = Steno_para(self.Form)
+        self.Form.show()
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Controller = Controller()
+    Controller.Show_HomeWindow()
+    sys.exit(app.exec_())
---
+++ Stenosis.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math as m
+from DB import arterytree
+import os
+
+def steno(M, R, G, **data):
+    #s = datetime.datetime.now()
+    RLCtru = np.zeros(shape=(128,4))
+    D = np.zeros(shape=(128))                            #easier to type # creating empty arrays of size 128,0
+    mu = M                                               #0.04  #blood viscosity
+    rho = R                                              #1.05       #blood density
+    G0 = G                                              #0.6   #Reflection coefficient
+    pi = m.pi                                            #value of pi
+    pos = []
+    values = []
+    for keys, value in data.items():
+        #print(keys,value)
+        pos.append(int(keys))
+        if value is None:
+            values.append(0)
+        else: values.append(value)
+    j = 0
+    for i in pos:
+        D[i] = values[j]
+        j = j +1
+    at = np.asarray(arterytree)
+    l = at[:, 0]                                          #assigning l with dataframe of 0th col of a variable
+    r = at[:, 1]                                          #assigning r with dataframe of 1st col of a variable
+    h = at[:, 2]                                          #assigning h with dataframe of 2nd col of a variable
+    E = np.multiply(at[:, 3], 10e+5)                      #assigning E with dataframe of 3rd col of a variable
+    RLCtru[:, 0] = np.true_divide(8*mu*l, np.power(pi*np.multiply(np.subtract(r, D), r), 4))   #8*mu*l ./ (pi*(r-D.*r).^4);
+    RLCtru[:, 1] = np.true_divide(np.multiply(l, rho), (pi*np.power(np.multiply(np.subtract(r, D), r), 2)))   # l.*rho ./ ((pi*(r-D.*r).^2))
+    RLCtru[:, 2] = np.multiply(np.multiply(3*pi, l), np.true_divide(np.power(np.multiply(np.subtract(r, D), r), 3), np.multiply(2*E, h)))  #3*pi.*l .* (r-D.*r).^3./(2*E.*h)
+    c0 = np.true_divide(np.multiply(E, h), rho*(np.multiply(np.subtract(r, D), r)))    #(E.*h./(rho*(r-D.*r))).^0.5
+    c0 = np.sign(c0) * (np.abs(c0)) ** (1 / 2)
+    R0 =  np.true_divide(rho*c0, np.power(pi*np.multiply(np.subtract(r, D), r), 2 ))     #rho*c0./(pi*(r-D.*r).^2)
+    RLCtru[:, 3] = R0 *(1+G0)/(1-G0)
+    path = os.path.join('Datas', 'RLCtru.txt')
+    np.savetxt(path, RLCtru, delimiter=',')
+
+
+if __name__ == "__main__":
+    stn_dat = {'0': None, '1': 3, '7': 4, '13':6, '3': 7, '11': 20, '10': 0, '51': 0, '46': 0,
+                '74': 0, '56': None, '70': 0, '62': 0, '63': 0, '108': 0, '109': 0, '102': 0, '107': 0, '96': 0, '92': 55}
+    steno(0.04, 1.05, 0.6, **stn_dat)
+
